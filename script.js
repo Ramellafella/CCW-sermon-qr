6e544d0f828c8742c6e56b66712aa1dd9ebf9e1e
Ramellafella <52977850+Ramellafella@users.noreply.github.com>
2026-06-20 15:37:54 +0100

    Updated script.js
    Added a button to redirect

--- script.js
+++ script.js
@@ -21,7 +21,19 @@ fetch(apiUrl)
     document.getElementById("passage").textContent = current.passage;
 
     const bibleUrl = "https://www.biblegateway.com/passage/?search=" + encodeURIComponent(current.passage);
+    const status= document.createElement("div");
 
+    status.innerHtml = `
+      <p>Opening Bible Gateway...</p>
+      <button id="goNow"> Tap to Continue</button>
+      `;
+    
+    document.body.appendChild(status);
+
+    document.getElementById("goNow").onclick = () => {
+      window.location.href = bibleUrl;
+    };
+    
     setTimeout(() => {
       window.location.href = bibleUrl;
     }, 3000);
